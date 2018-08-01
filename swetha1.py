c12c51d495e3be8676ce0a462d32f50f986dadca
saiswethak <41769608+saiswethak@users.noreply.github.com>
2018-08-01 15:42:51 +0530

    Update swetha1.py

--- swetha1.py
+++ swetha1.py
@@ -1,10 +1,8 @@
-num,num1,num2=raw_input().split()
-num,num1,num2=int(num),int(num1),int(num2)
-if(num>num1) and (num>num1):
+num,num1,num2=raw_input().split
+num,num1,num2=int(num) , int(num1) , int(num2)
+if(num>num1) and (num>num2):
 	print(num)
 elif(num1>num2):
 	print(num1)
 else:
 	print(num2)
-		
-		
