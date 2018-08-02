ecfceac7f3694bba336bd61d44b1c1731a9929e3
saiswethak <41769608+saiswethak@users.noreply.github.com>
2018-08-02 14:21:16 +0530

    Update swetha2.py

--- swetha2.py
+++ swetha2.py
@@ -1,5 +1,11 @@
 a=int(input(" "))
-if (a % 4) == 0:
-	print("yes")
+if(a % 4==0):
+    if(a % 100==0):
+        if(a % 400==0):
+            print("yes")
+        else:
+            print("no")
+    else:
+        print("yes")
 else:
-	print("no")
+     print("no")
