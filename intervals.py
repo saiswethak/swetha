2503392df2830ef075d54009a7efd806bb9cc50c
saiswethak <41769608+saiswethak@users.noreply.github.com>
2018-08-11 19:37:59 +0530

    Update intervals.py

--- intervals.py
+++ intervals.py
@@ -1,8 +1,14 @@
 lower,upper=map(int,input().split())
 for i in range(lower,upper+1):
-    count=0
+    count=0;l=0;c=0
     for j in range(1,i+1):
         if(i%j==0):
             count+=1
     if(count==2):
-        print(i,end=" ")
+        if(c==0):
+            print(i,end="")
+        else:
+            print("",end=" ")
+            print(i,end="")
+        l+=1
+
