66203b1243b37ea7671cfc6c1011bfeae5544fc4
saiswethak <41769608+saiswethak@users.noreply.github.com>
2018-08-13 18:38:42 +0530

    Update meadian.py

--- meadian.py
+++ meadian.py
@@ -1,9 +1,9 @@
 def median(l,n):
-    1,sort()
+    l,sort()
     if n%2==0:
-        return 1[n/2]
+        return l[n/2]
     else:
-        return (1[n/2-1]+1[n/2])/2
+        return (l[n/2-1]+l[n/2])/2
     n=int(raw_input())
-    1=[int(x) for x in raw_input().split()]
+    l=[int(x) for x in raw_input().split()]
     print (median(l,n-1))
