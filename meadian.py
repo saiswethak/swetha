31fdca567566683880d551d19ae6c8af74da7229
saiswethak <41769608+saiswethak@users.noreply.github.com>
2018-08-13 18:36:30 +0530

    Create meadian.py

---
+++ meadian.py
@@ -0,0 +1,9 @@
+def median(l,n):
+    1,sort()
+    if n%2==0:
+        return 1[n/2]
+    else:
+        return (1[n/2-1]+1[n/2])/2
+    n=int(raw_input())
+    1=[int(x) for x in raw_input().split()]
+    print (median(l,n-1))
