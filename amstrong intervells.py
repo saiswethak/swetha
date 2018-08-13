5982af94456d5fcc978b675e1dbe640b3075005e
saiswethak <41769608+saiswethak@users.noreply.github.com>
2018-08-13 15:36:43 +0530

    Update amstrong intervells.py

--- amstrong intervells.py
+++ amstrong intervells.py
@@ -1,11 +1,17 @@
-s,p=map(int,raw_input().split())
+s,p=map(int,input().split())
+count=0
 for i in range(s,p):
-    l=len(str(i))
-    sum=0
+    le=len(str(i))
+    sum1=0
     temp=i
     while i>0:
         x=i%10
-        sum=sum+(x**1)
-        i=i/10
-    if(sum==temp):
-        print(temp)
+        sum1=sum1+(x**le)
+        i=i//10
+    if(sum1==temp):
+        if(count==0):
+            print(temp,end="")
+        else:
+            print("",end=" ")
+            print(temp,end="")
+        count=count+1
