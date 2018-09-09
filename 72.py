63cbe77a527af3b62ff1a750c74b3533d9a650a0
saiswethak <41769608+saiswethak@users.noreply.github.com>
2018-09-09 16:18:23 +0530

    Update 72.py

--- 72.py
+++ 72.py
@@ -1,6 +1,6 @@
-m=input()
-for i in range(0,len(m)):
-    if m[i]=='a' or m[i]=='e' or m[i]=='i' or m[i]=='o' or m[i]=='u':
+sp=input()
+for i in range(0,len(sp)):
+    if sp[i]=='a' or sp[i]=='e' or sp[i]=='i' or sp[i]=='o' or sp[i]=='u':
         print("yes")
         break
 else:print("no")
